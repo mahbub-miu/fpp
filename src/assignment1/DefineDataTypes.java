6ac7702f53f8981a1dc4b8770fec0dc4d71e72b0
Mahbub Hossain <mahbub.hossain@miu.com>
2024-04-03 15:28:26 -0500

    3rd Commit for assignment-1

--- src/assignment1/DefineDataTypes.java
+++ src/assignment1/DefineDataTypes.java
@@ -5,9 +5,13 @@ public class DefineDataTypes {
 
 
 
-        long myRandom = RandomNumbers.getRandomInt(0, 2_000_000_000);
+        int myRandom = RandomNumbers.getRandomInt(0, 2_000_000_000);
         System.out.println(myRandom);
-
+        /* following if block is to check which data type should be appropriate to store the generated random number
+        Here we don't need to check long data type as the method is returning int.
+        So no possibility to have larger number than integer max value.
+        Also we don't need to consider the float or double data types for the same reason.
+         */
         if(myRandom<=Byte.MAX_VALUE){
             System.out.println("Data type is : Byte");
         }
@@ -15,7 +19,7 @@ public class DefineDataTypes {
             System.out.println("Data type is : Short");
         }
         else
-            System.out.println("Data type is : Integer"); // no need to check long data type as the method is returning int. So no possibility to have larger number than integer max value
+            System.out.println("Data type is : Integer");
 
     }
 
--- src/assignment2/Prog1.java
+++ src/assignment2/Prog1.java
@@ -1,8 +1,16 @@
 package src.assignment2;
 
+import src.assignment1.RandomNumbers;
+
 public class Prog1 {
     public static void main(String...args){
-        double paiValue=Math.PI;
+        int x = RandomNumbers.getRandomInt(1, 9);
+        int y=RandomNumbers.getRandomInt(3,14);
+        //double result1=Math.pow(Math.PI,x);
+        //double result2=Math.pow(y,Math.PI);
+
+        System.out.println("The result of PI raised to the power of "+ x+" : "+Math.pow(Math.PI,x));
+        System.out.println("The result of "+ y +" raised to the power of PI :"+Math.pow(y,Math.PI));
 
     }
 }
