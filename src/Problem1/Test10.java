d2875b68f4dd25e6fd56a34d6f9cc2e4d5112bf0
Mahbub Hossain <mahbub.hossain@miu.com>
2024-04-24 19:27:11 -0500

    Final commit for assignment 10

--- src/Problem1/Test10.java
+++
@@ -1,9 +0,0 @@
-package src.Problem1;
-
-public class Test10 {
-    public static void main(String[] args) {
-
-
-
-    }
-}
---
+++ src/ignore/IgnoreThisFile.java
@@ -0,0 +1,5 @@
+package src.ignore;
+
+public class IgnoreThisFile {
+    // this file is created just to maintain the structure of the scr package
+}
